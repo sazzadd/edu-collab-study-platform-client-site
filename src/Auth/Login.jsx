7b9912677877190d423cdb3199ad05ab6cbaddd4
sazzad <sazzadador@gmail.com>
2025-01-14 20:56:45 +0600

    INSTALL FIREBASE

--- src/Auth/Login.jsx
+++ src/Auth/Login.jsx
@@ -1,9 +1,35 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { FaFacebook, FaGithub, FaGoogle } from "react-icons/fa";
-
+import {
+  LoadCanvasTemplate,
+  loadCaptchaEnginge,
+  validateCaptcha,
+} from "react-simple-captcha";
+import { toast } from "react-toastify";
 const Login = () => {
   const [passwordVisible, setPasswordVisible] = useState(false);
-
+  const [error, setError] = useState({});
+  const captchaRef = useRef(null);
+  const [disabled, setDisabled] = useState(true);
+  const [captchaValidated, setCaptchaValidated] = useState(false);
+  useEffect(() => {
+    try {
+      loadCaptchaEnginge(6);
+    } catch (err) {
+      console.error("Captcha Engine Error:", err);
+    }
+  }, []);
+  const handleValidateCaptcha = () => {
+    const user_captcha_value = captchaRef.current.value;
+    if (validateCaptcha(user_captcha_value)) {
+      setDisabled(false);
+      setCaptchaValidated(true);
+      toast.success("Captcha validated!");
+    } else {
+      setDisabled(true);
+      toast.error("Captcha validation failed.");
+    }
+  };
   return (
     <div className="min-h-screen flex justify-center items-center bg-white">
       <div className="bg-white shadow-lg rounded-lg flex flex-col-reverse md:flex-row w-[90%] max-w-4xl">
@@ -22,14 +48,19 @@ const Login = () => {
           </h2>
           <p className="text-gray-600 mb-6">
             Don't have an account?{" "}
-            <a href="/auth/register" className="text-[#10b981] font-semibold underline">
+            <a
+              href="/auth/register"
+              className="text-[#10b981] font-semibold underline"
+            >
               Register here
             </a>
           </p>
           <form className="space-y-4">
             {/* Email */}
             <div>
-              <label className="block text-gray-700 text-sm font-medium">Email</label>
+              <label className="block text-gray-700 text-sm font-medium">
+                Email
+              </label>
               <input
                 type="email"
                 placeholder="Your email"
@@ -38,7 +69,9 @@ const Login = () => {
             </div>
             {/* Password */}
             <div>
-              <label className="block text-gray-700 text-sm font-medium">Password</label>
+              <label className="block text-gray-700 text-sm font-medium">
+                Password
+              </label>
               <div className="relative">
                 <input
                   type={passwordVisible ? "text" : "password"}
@@ -55,16 +88,44 @@ const Login = () => {
               </div>
             </div>
             {/* Submit Button */}
-            <button
+            <div className="mb-4">
+              <label className="block text-gray-700 text-sm font-bold mb-2">
+                Captcha
+              </label>
+              <LoadCanvasTemplate />
+              <input
+                type="text"
+                name="captcha"
+                ref={captchaRef}
+                placeholder="Type here"
+                className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-[#10b981] transition text-sm"
+              />
+              <button
+                type="button"
+                onClick={handleValidateCaptcha}
+                className="btn mt-5 btn-outline btn-xs px-3 py-1 text-xs font-medium rounded-full border-2 border-green-500 text-green-500 hover:bg-green-500 hover:text-white shadow-md hover:shadow-lg transition-transform duration-300 ease-in-out transform hover:scale-110"
+              >
+                Validate
+              </button>
+            </div>
+            {/*  */}
+            <input
+              disabled={disabled}
+              className={`w-full py-2 rounded-lg text-white transition-opacity duration-300 ${
+                disabled
+                  ? "bg-gray-300 cursor-not-allowed opacity-70"
+                  : "w-full bg-[#10b981] text-white py-2 px-4 rounded-md hover:bg-[#0e9b76] transition text-sm hover:bg-blue-500 cursor-pointer"
+              }`}
               type="submit"
-              className="w-full bg-[#10b981] text-white py-2 px-4 rounded-md hover:bg-[#0e9b76] transition text-sm"
-            >
-              Login
-            </button>
+              value="Login"
+            />
+            
           </form>
           {/* Social Media Buttons */}
           <div className="mt-6 text-center">
-            <p className="text-gray-600 mb-2 text-sm">Login with Social Media</p>
+            <p className="text-gray-600 mb-2 text-sm">
+              Login with Social Media
+            </p>
             <div className="flex justify-center space-x-4">
               <button className="bg-[#10b981] text-white p-2 rounded-full hover:bg-[#0e9b76] transition">
                 <FaFacebook size={16} />
--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,11 @@ import { RouterProvider } from "react-router-dom";
 import "./index.css";
 import { router } from "./Routes/Routes";
 
+import { ToastContainer } from "react-toastify";
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    <ToastContainer />
     <RouterProvider router={router} />
   </React.StrictMode>
 );
